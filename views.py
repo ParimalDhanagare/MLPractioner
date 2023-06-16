5ef002604c2b9a73392fbef1123565b772067d94
ParimalDhanagare <136802607+ParimalDhanagare@users.noreply.github.com>
2023-06-16 17:13:53 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+import streamlit as st
+
+from joblib import load
+model = load('RandomForest.pkl','rb')
+
+def predictor(request):
+    if request.method == 'POST':
+        pH = request.POST['pH']
+        Hardness = request.POST['Hardness']
+        Solids = request.POST['Solids']
+        Chloramines = request.POST['Chloramines']
+        Sulfate = request.POST['Sulfate']
+        Conductivity = request.POST['Conductivity']
+        Organic_carbon= request.POST['Organic_carbon']
+        Trihalomethanes = request.POST['Trihalomethanes']
+        Turbidity = request.POST['Turbidity']
+        y_pred = model.predict([[pH, Hardness, Solids,Chloramines,Sulfate,Conductivity,Organic_carbon,Trihalomethanes,Turbidity]])
+        if y_pred[0] == 0:
+            y_pred = 'Not Potable'
+        elif y_pred[0] == 1:
+            y_pred = 'Potable'
+        return render(request, 'main.html', {'result' : y_pred})
+    return render(request, 'main.html')
+if __name__ == '__main__':
+    predictor()
