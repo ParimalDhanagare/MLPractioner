f18deede4b429a9f4c4c9c56f8083b785b369dbc
ParimalDhanagare <136802607+ParimalDhanagare@users.noreply.github.com>
2023-08-26 12:24:17 +0530

    Update application.py

--- IMDB_Movie_Review_Sentiment_Analysis/application.py
+++ IMDB_Movie_Review_Sentiment_Analysis/application.py
@@ -4,10 +4,10 @@ import streamlit as st
 
 loaded_model=pickle.load(open("IMDB_Movie_Review_Sentiment_Analysis/logistic_regression_model.pkl",'rb'))
 
-def review_sentiment():
-    # input_data_as_numpy_array=np.asarray(input_data,dtype='object')
-    # input_data_reshaped=input_data_as_numpy_array.reshape(1,-1)
-    prediction=loaded_model.predict()
+def review_sentiment(input_data):
+    input_data_as_numpy_array=np.asarray(input_data,dtype='object')
+    input_data_reshaped=input_data_as_numpy_array.reshape(1,-1)
+    prediction=loaded_model.predict(input_data_reshaped)
     print(prediction)
 
     if (prediction[0]==0):
