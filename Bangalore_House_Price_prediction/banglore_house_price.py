18134ed6bc3d232ba1d340916c17b74de50381bc
ParimalDhanagare <136802607+ParimalDhanagare@users.noreply.github.com>
2023-08-27 12:00:50 +0530

    banglore_house_price.py

--- Bangalore_House_Price_prediction/banglore_house_price.py
+++ Bangalore_House_Price_prediction/banglore_house_price.py
@@ -5,9 +5,9 @@ import streamlit as st
 loaded_model=pickle.load(open("Bangalore_House_Price_prediction/bangalore_house_price_prediction.pkl",'rb'))
 
 def predict_price(input_data):
-    # input_data_as_numpy_array=np.asarray(input_data)
-    # input_data_reshaped=input_data_as_numpy_array.reshape(1,-1)
-    prediction=loaded_model.predict(input_data)
+    input_data_as_numpy_array=np.asarray(input_data)
+    input_data_reshaped=input_data_as_numpy_array.reshape(1,-1)
+    prediction=loaded_model.predict(input_data_reshaped)
     print(prediction)
 
 
