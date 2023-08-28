ab24bf03bbe2b284d8e5de36f1db7236d7fa0e5d
ParimalDhanagare <136802607+ParimalDhanagare@users.noreply.github.com>
2023-08-28 11:45:50 +0530

    application.py

--- IMDB_Movie_Review_Sentiment_Analysis/application.py
+++ IMDB_Movie_Review_Sentiment_Analysis/application.py
@@ -31,12 +31,9 @@ def main():
     test_result=''
 
     if st.button('Predict movie sentiment'):
-        if (movie_name == "") or (movie_review == ""):
-            st.success("Please fill the remaining details.")
         if (movie_name == "") and (movie_review == ""):
             st.success("Both the fields are required..")
         else:
-            
             test_result=review_sentiment([movie_review])
     
             st.success(test_result)
